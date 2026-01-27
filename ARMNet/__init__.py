5af51b13f0a0ae9862b50f6df9e80d77bbb42ba3
sonvth <sonvth.clone@gmail.com>
2026-01-27 21:04:45 +0900

    Clean up: Keep only components related to DenoisingARM

--- ARMNet/__init__.py
+++ ARMNet/__init__.py
@@ -1,39 +1,11 @@
-# ARMNet Package
-from .model import ARMNet
-from .diffusion_model import DiffusionARM, DiffusionManager
-from .diffusion_3d_model import DiffusionARM3D, DiffusionManager3D
-from .cvae_model import HierarchicalCVAE
+# ARMNet Package - DenoisingARM Branch
 from .denoising_model import DenoisingARM
-from .loss import PeakFocusedLoss, CVAELoss
-
-# Optional: Graph models require torch_geometric
-try:
-    from .flow_graph_model import FlowGraphARM
-    from .diffusion_graph_model import DiffusionGraphARM
-    _has_graph = True
-except ImportError:
-    _has_graph = False
-    FlowGraphARM = None
-    DiffusionGraphARM = None
-
-# Flow 3D Hybrid model (uses torch_geometric optionally)
-from .flow_3d_hybrid_model import Flow3DHybridARM
-from .flow_3d_loss import FlowMatching3DLoss, Metrics3D1D
+from .loss import PeakFocusedLoss
+from .diffusion_model import ResBlock, CrossAttentionBlock
 
 __all__ = [
-    'ARMNet',
-    'DiffusionARM',
-    'DiffusionManager',
-    'DiffusionARM3D',
-    'DiffusionManager3D',
-    'HierarchicalCVAE',
     'DenoisingARM',
     'PeakFocusedLoss',
-    'CVAELoss',
-    'Flow3DHybridARM',
-    'FlowMatching3DLoss',
-    'Metrics3D1D',
+    'ResBlock',
+    'CrossAttentionBlock',
 ]
-
-if _has_graph:
-    __all__.extend(['FlowGraphARM', 'DiffusionGraphARM'])
